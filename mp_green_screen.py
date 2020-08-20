5575a10c291d4f1c455328c4d54900606367139c
Carlos Co <carloscomail@gmail.com>
2020-08-20 18:29:50 -0400

    Initial commit

--- mp_green_screen.py
+++ mp_green_screen.py
@@ -1,5 +1,6 @@
 import time, os, configparser, argparse
 from sys import platform
+from itertools import cycle
 import multiprocessing as mp
 import cv2
 import PySimpleGUI as sg
@@ -9,15 +10,48 @@ BUFFER_SIZE = 1280*960*3
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--ini', type=str)
+parser.add_argument('-f', '--find_cam', help='cycle through cameras', action='store_true')
 args = parser.parse_args()
 
 config_file = args.ini if args.ini else 'config.ini'
 config = configparser.ConfigParser()
 
+def find_cam(frame, new_frame, dim):
+
+    ids = cycle(range(6))
+    while True:
+        id = next(ids)
+        cam = cv2.VideoCapture(id)
+        try:
+            ret, img = cam.read()
+            dim.acquire()
+            hsv = cv2.cvtColor(np.fliplr(img)[dim.T:-dim.B, dim.L:-dim.R,:],cv2.COLOR_BGR2HSV)
+            mask=np.zeros((hsv.shape[0],hsv.shape[1]))
+            np.logical_or(np.less(hsv[:,:,0],dim.hue_loPass), mask, out=mask)
+            np.logical_or(np.greater(hsv[:,:,0],dim.hue_hiPass), mask, out=mask)
+            np.logical_or(np.less(hsv[:,:,1], dim.sat_loPass),mask, out=mask)
+            np.logical_or(np.less(hsv[:,:,2], dim.bright_loPass),mask, out=mask)
+
+            frame_array = np.frombuffer(frame,dtype=c_uint8)
+            img_resize = cv2.resize(mask[:,:,None]*np.fliplr(img)[dim.T:-dim.B, dim.L:-dim.R,:],
+                                    dsize=(int((img.shape[1]-dim.L-dim.R)/dim.scale),
+                                           int((img.shape[0]-dim.T-dim.B)/dim.scale)),
+                                    interpolation=cv2.INTER_CUBIC)
+            data = cv2.imencode('.png', np.rot90(img_resize, dim.rotate))[1][:,0]
+            dim.release()
+            frame_array[:data.shape[0]] = data
+            new_frame.value=True
+            print(f'VALID ID: {id}', sep='')
+            time.sleep(1)
+        except:
+            print(f'Invalid camera ID: {id}', sep='')
+        print()
+
 
 def update_frame(frame, new_frame, dim):
 
     cam = cv2.VideoCapture(dim.ID)
+
     while True:
         ret, img = cam.read()
         dim.acquire()
@@ -37,6 +71,7 @@ def update_frame(frame, new_frame, dim):
         dim.release()
         frame_array[:data.shape[0]] = data
         new_frame.value=True
+        time.sleep(0.1)
 
 class Dim(Structure):
     _fields_ = [('ID', c_int), ('T', c_int), ('B', c_int), ('L', c_int), ('R', c_int), ('scale', c_float),
@@ -84,10 +119,17 @@ if __name__ == '__main__':
     dim = mp.Value(Dim)
     read_config(dim)
 
+
+
     monitor_config_proc = ctx.Process(target=monitor_config_change, args=(dim,), daemon=True)
     monitor_config_proc.start()
-    update_frame_proc = ctx.Process(target=update_frame, args=(frame, new_frame, dim), daemon=True)
-    update_frame_proc.start()
+
+    if args.find_cam:
+        find_cam_proc = ctx.Process(target=find_cam, args=(frame, new_frame, dim), daemon=True)
+        find_cam_proc.start()
+    else:
+        update_frame_proc = ctx.Process(target=update_frame, args=(frame, new_frame, dim), daemon=True)
+        update_frame_proc.start()
 
 
     while True:
