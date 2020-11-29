89cd0da77be8f37e0ff6f9f3e1419136156a7934
Carlos Co <carloscomail@gmail.com>
2020-11-29 08:15:34 -0500

    Fixed filtering, set #00FF00 as transparent color

--- float_cam.py
+++ float_cam.py
@@ -8,8 +8,9 @@ from ctypes import c_bool, c_uint8, c_int, Structure, c_float
 from layout import make_layout
 BUFFER_SIZE = 1920*1080*3
 
+
 def find_cam():
-    valid_id=[]
+    valid_id = []
     for i in range(8):
         cam = cv2.VideoCapture(i)
         try:
@@ -25,13 +26,15 @@ def update_frame(frame, new_frame, dim, valid_ids):
     print(f'update frame valid: {valid_ids}')
     old_ID = dim.ID
     cam = cv2.VideoCapture(valid_ids[dim.ID])
-    scale=2
+    scale = 2
     w = cam.get(3)
     h = cam.get(4)
     l = int(w*dim.L/100)
     r = int(l + (w-l)*dim.W/100)
     t = int(h*dim.T/100)
     b = int(t + (h-t)*dim.H/100)
+    mask = np.zeros((int((b-t)/scale), int((r-l)/scale)))
+    transparent = np.array([0, 255, 0], dtype=c_uint8)
 
     while True:
         try:
@@ -49,37 +52,47 @@ def update_frame(frame, new_frame, dim, valid_ids):
                 r = int(l + (w-l)*dim.W/100)
                 t = int(h*dim.T/100)
                 b = int(t + (h-t)*dim.H/100)
-                scale=dim.scale
-                dim.change=False
-            hsv = cv2.cvtColor(np.fliplr(img)[t:b, l:r,:],cv2.COLOR_BGR2HSV)
-            mask=np.zeros((hsv.shape[0],hsv.shape[1]))
-            np.logical_or(np.less(hsv[:,:,0],dim.hue_loPass), mask, out=mask)
-            np.logical_or(np.greater(hsv[:,:,0],dim.hue_hiPass), mask, out=mask)
-            np.logical_or(np.less(hsv[:,:,1], dim.sat_loPass),mask, out=mask)
-            np.logical_or(np.less(hsv[:,:,2], dim.bright_loPass),mask, out=mask)
+                scale = dim.scale
+                mask = np.zeros((int((b-t)/scale), int((r-l)/scale)))
+                dim.change = False
+            img = cv2.resize(img[t:b, l:r, :],
+                             dsize=(int((r-l)/scale),
+                                    int((b-t)/scale)),
+                             interpolation=cv2.INTER_CUBIC)
+            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            mask.fill(0)
+            np.logical_or(
+                np.less(hsv[:, :, 0], dim.hue_loPass), mask, out=mask)
+            np.logical_or(np.greater(
+                hsv[:, :, 0], dim.hue_hiPass), mask, out=mask)
+            np.logical_or(
+                np.less(hsv[:, :, 1], dim.sat_loPass), mask, out=mask)
+            np.logical_or(
+                np.less(hsv[:, :, 2], dim.bright_loPass), mask, out=mask)
 
-            frame_array = np.frombuffer(frame,dtype=c_uint8)
+            frame_array = np.frombuffer(frame, dtype=c_uint8)
+
+            img = np.where(mask[:, :, None], img, transparent[None, None, :])
             if dim.hflip:
-                img=np.fliplr(img)
-
-            img_resize = cv2.resize(mask[:,:,None]*img[t:b, l:r,:],
-                                    dsize=(int((r-l)/scale),
-                                           int((b-t)/scale)),
-                                    interpolation=cv2.INTER_CUBIC)
-            data = cv2.imencode('.png', np.rot90(img_resize, dim.rotate))[1][:,0]
+                img = np.fliplr(img)
+            data = cv2.imencode('.png', np.rot90(
+                img, dim.rotate))[1][:, 0]
             dim.release()
             frame_array[:data.shape[0]] = data
-            new_frame.value=True
+            new_frame.value = True
         except:
-            dim.change=False
+            dim.change = False
             dim.release()
             pass
         time.sleep(0.03)
 
+
 class Dim(Structure):
     _fields_ = [('change', c_bool), ('ID', c_int), ('T', c_int), ('H', c_int), ('L', c_int), ('W', c_int), ('scale', c_float),
-                ('rotate', c_int), ('hflip', c_bool), ('hue_loPass', c_int), ('hue_hiPass', c_int), ('sat_loPass', c_int),
+                ('rotate', c_int), ('hflip', c_bool), ('hue_loPass',
+                                                       c_int), ('hue_hiPass', c_int), ('sat_loPass', c_int),
                 ('bright_loPass', c_int)]
+
 
 def config(dim, window2, terminate):
 
@@ -88,84 +101,80 @@ def config(dim, window2, terminate):
         if event != sg.TIMEOUT_KEY:
             print(event, values)
             dim.acquire()
-            dim.change=True
-            if event=='L':
+            dim.change = True
+            if event == 'L':
                 dim.L = int(values['L'])
-            elif event=='W':
+            elif event == 'W':
                 dim.W = int(values['W'])
-            elif event=='ID':
+            elif event == 'ID':
                 dim.ID = int(values['ID'])
-            elif event=='T':
+            elif event == 'T':
                 dim.T = int(values['T'])
-            elif event=='H':
+            elif event == 'H':
                 dim.H = 100-int(values['H'])
-            elif event=='rotate':
+            elif event == 'rotate':
                 dim.rotate = int(values['rotate'])
-            elif event=='hflip':
+            elif event == 'hflip':
                 dim.hflip = bool(values['hflip'])
-            elif event=='scale':
-                dim.scale=values['scale']
-            elif event=='hue_loPass':
-                dim.hue_loPass=int(values['hue_loPass'])
-            elif event=='hue_hiPass':
-                dim.hue_hiPass=int(values['hue_hiPass'])
-            elif event=='sat_loPass':
-                dim.sat_loPass=int(values['sat_loPass'])
-            elif event=='bright_loPass':
-                dim.bright_loPass=int(values['bright_loPass'])
+            elif event == 'scale':
+                dim.scale = values['scale']
+            elif event == 'hue_loPass':
+                dim.hue_loPass = int(values['hue_loPass'])
+            elif event == 'hue_hiPass':
+                dim.hue_hiPass = int(values['hue_hiPass'])
+            elif event == 'sat_loPass':
+                dim.sat_loPass = int(values['sat_loPass'])
+            elif event == 'bright_loPass':
+                dim.bright_loPass = int(values['bright_loPass'])
             dim.release()
         if event is None or event == sg.WIN_CLOSED or event == 'Exit':
-            terminate.value=True
+            terminate.value = True
             break
 
 
-
 def dim_init(dim):
-    dim.ID=0
-    dim.L=0
-    dim.W=100
-    dim.T=0
-    dim.H=100
-    dim.scale=2.
-    dim.rotate=0
-    dim.hflip=True
+    dim.ID = 0
+    dim.L = 0
+    dim.W = 100
+    dim.T = 0
+    dim.H = 100
+    dim.scale = 2.
+    dim.rotate = 0
+    dim.hflip = True
     dim.hue_loPass = 40
     dim.hue_hiPass = 90
     dim.sat_loPass = 50
-    dim.bright_loPass =50
+    dim.bright_loPass = 50
+
 
 if __name__ == '__main__':
 
-    if platform =='win32':
-        window = sg.Window('Demo Application - OpenCV Integration', [[sg.Image(filename='', key='image', background_color='black')],],
-                           transparent_color='black', no_titlebar=True, grab_anywhere=True, return_keyboard_events=True,
-                           keep_on_top=True, force_toplevel=True, element_padding=(0,0),margins=(0,0) )
+    if platform == 'win32':
+        window = sg.Window('FloatCam', [[sg.Image(filename='', key='image', background_color='#00FF00')], ],
+                           transparent_color='#00FF00', no_titlebar=True, grab_anywhere=True, return_keyboard_events=True,
+                           keep_on_top=True, force_toplevel=True, element_padding=(0, 0), margins=(0, 0))
     else:
-        window = sg.Window('Demo Application - OpenCV Integration', [[sg.Image(filename='', key='image', background_color='black')],],
+        window = sg.Window('FloatCam', [[sg.Image(filename='', key='image', background_color='black')], ],
                            no_titlebar=True, grab_anywhere=True, return_keyboard_events=True,
-                           keep_on_top=True, force_toplevel=True, element_padding=(0,0),margins=(0,0) )
-
-
+                           keep_on_top=True, force_toplevel=True, element_padding=(0, 0), margins=(0, 0))
 
     ctx = mp.get_context('spawn')
     frame = mp.RawArray(c_uint8, BUFFER_SIZE)
     new_frame = ctx.Value(c_bool, False)
     terminate = ctx.Value(c_bool, False)
     dim = mp.Value(Dim)
-    valid_ids=find_cam()
-    # valid_ids=[1,2,3]
-    # print(f'valid: {valid_ids}')
+    valid_ids = find_cam()
     dim_init(dim)
-
 
     window2 = sg.Window('FloatCam Controls', make_layout(valid_ids))
 
-    config_proc = ctx.Process(target=config, args=(dim, window2, terminate), daemon=True)
+    config_proc = ctx.Process(target=config, args=(
+        dim, window2, terminate), daemon=True)
     config_proc.start()
 
-    update_frame_proc = ctx.Process(target=update_frame, args=(frame, new_frame, dim, valid_ids), daemon=True)
+    update_frame_proc = ctx.Process(target=update_frame, args=(
+        frame, new_frame, dim, valid_ids), daemon=True)
     update_frame_proc.start()
-
 
     while True:
         event, values = window.Read(timeout=10)
@@ -173,6 +182,6 @@ if __name__ == '__main__':
             break
 
         if new_frame.value:
-            frame_array = np.frombuffer(frame,dtype=c_uint8)
+            frame_array = np.frombuffer(frame, dtype=c_uint8)
             window.FindElement('image').Update(data=frame_array.tobytes())
-            new_frame.value=False
+            new_frame.value = False
--- layout.py
+++ layout.py
@@ -6,45 +6,43 @@ sg.theme_slider_color('#303050')
 
 def make_layout(valid_ids):
 
-    top_height_layout = [[sg.Text('Top', font=('',15),pad=((32,0),(15,0))), sg.Text('Height', font=('',15), pad=((18,0),(15,0)))],
-                         [sg.Slider(range=(1, 100), orientation='v', size=(16, 25), pad=((10, 0),(10,25)), default_value=0, key='T',enable_events=True),
-                          sg.Slider(range=(1, 100), orientation='v', size=(16, 25), pad=((10,5),(10,25)), default_value=0, key='H',enable_events=True)]
+    top_height_layout = [[sg.Text('Top', font=('', 15), pad=((32, 0), (15, 0))), sg.Text('Height', font=('', 15), pad=((18, 0), (15, 0)))],
+                         [sg.Slider(range=(1, 100), orientation='v', size=(16, 25), pad=((10, 0), (10, 25)), default_value=0, key='T', enable_events=True),
+                          sg.Slider(range=(1, 100), orientation='v', size=(16, 25), pad=((10, 5), (10, 25)), default_value=0, key='H', enable_events=True)]
                          ]
 
     left_width_layout = [[
-        sg.Text('Left', font=('',15), pad=((30,0),(8,0))), sg.Slider(range=(1, 100), orientation='h', size=(46, 20),pad=((10,26),10),
-                                                                 default_value=0, key='L',enable_events=True)],
-        [sg.Text('Width', font=('',15), pad=((30,0),(0,0))), sg.Slider(range=(1, 100), orientation='h', size=(46, 20), pad=((10,26),(0,10)),
-                                                                   default_value=100, key='W',enable_events=True)]]
+        sg.Text('Left', font=('', 15), pad=((30, 0), (8, 0))), sg.Slider(range=(1, 100), orientation='h', size=(46, 20), pad=((10, 26), 10),
+                                                                         default_value=0, key='L', enable_events=True)],
+        [sg.Text('Width', font=('', 15), pad=((30, 0), (0, 0))), sg.Slider(range=(1, 100), orientation='h', size=(46, 20), pad=((10, 26), (0, 10)),
+                                                                           default_value=100, key='W', enable_events=True)]]
 
-    cam_flip_layout = [sg.Text('Camera', font=('',18), pad=(5,(16,27))),
-                       sg.Spin(list(range(0,len(valid_ids))), initial_value=0, font=('',20), pad=((0,30),(18,27)), key='ID',
+    cam_flip_layout = [sg.Text('Camera', font=('', 18), pad=(5, (16, 27))),
+                       sg.Spin(list(range(0, len(valid_ids))), initial_value=0, font=('', 20), pad=((0, 30), (18, 27)), key='ID',
                                background_color='#303050', text_color='#FFFFFF', enable_events=True),
-                       sg.Checkbox(default=True, text='H-Flip',  font=('',18), pad=((0,30),(18,27)), key='hflip',enable_events=True)]
+                       sg.Checkbox(default=True, text='H-Flip',  font=('', 18), pad=((0, 30), (18, 27)), key='hflip', enable_events=True)]
 
     chroma_layout = [
-        [sg.Text('Hue LoPass', font=('',15), pad=(10,(12,0))),
-         sg.Slider(range=(1, 255),orientation='h',size=(20, 20), default_value=40,key='hue_loPass',enable_events=True)],
-        [sg.Text('Hue HiPass', font=('',15), pad=(10,(12,0))),
-         sg.Slider(range=(1, 255), orientation='h',size=(20, 20), default_value=90, key='hue_hiPass',enable_events=True)],
-        [sg.Text('Sat LoPass', font=('',15), pad=(10,(12,0))),
-         sg.Slider(range=(1, 255), orientation='h',size=(20, 20), default_value=50, key='sat_loPass',enable_events=True)],
-        [sg.Text('Bright LoPass', font=('',15), pad=(10,(12,0))),
-         sg.Slider(range=(1, 255), orientation='h',size=(20, 20), default_value=50, key='bright_loPass',enable_events=True)],
-        [sg.Text('Rotate', font=('',15), pad=(10,(12,0))),
-         sg.Slider(range=(0, 3),orientation='h',size=(20, 20), default_value=0, key='rotate', enable_events=True)],
-        [sg.Text('Scale', font=('',15), pad=(10,(12,0))),
-         sg.Slider(range=(1, 24),orientation='h',size=(20, 20), default_value=2, resolution = 0.25, key='scale', enable_events=True)],
+        [sg.Text('Hue LoPass', font=('', 15), pad=(10, (12, 0))),
+         sg.Slider(range=(0, 255), orientation='h', size=(20, 20), default_value=40, key='hue_loPass', enable_events=True)],
+        [sg.Text('Hue HiPass', font=('', 15), pad=(10, (12, 0))),
+         sg.Slider(range=(0, 255), orientation='h', size=(20, 20), default_value=90, key='hue_hiPass', enable_events=True)],
+        [sg.Text('Sat LoPass', font=('', 15), pad=(10, (12, 0))),
+         sg.Slider(range=(0, 255), orientation='h', size=(20, 20), default_value=50, key='sat_loPass', enable_events=True)],
+        [sg.Text('Bright LoPass', font=('', 15), pad=(10, (12, 0))),
+         sg.Slider(range=(0, 255), orientation='h', size=(20, 20), default_value=50, key='bright_loPass', enable_events=True)],
+        [sg.Text('Rotate', font=('', 15), pad=(10, (12, 0))),
+         sg.Slider(range=(0, 3), orientation='h', size=(20, 20), default_value=0, key='rotate', enable_events=True)],
+        [sg.Text('Scale', font=('', 15), pad=(10, (12, 0))),
+         sg.Slider(range=(1, 24), orientation='h', size=(20, 20), default_value=2, resolution=0.25, key='scale', enable_events=True)],
     ]
 
-    chroma_camera_layout = [[sg.Column(chroma_layout, element_justification='right', pad=(20,15))],
+    chroma_camera_layout = [[sg.Column(chroma_layout, element_justification='right', pad=(20, 15))],
                             cam_flip_layout]
 
-
-
-    layout= [[sg.Column(left_width_layout, element_justification='right')],
-             [sg.Column(top_height_layout, element_justification='center'),
-              sg.Column(chroma_camera_layout, element_justification='center')]
-             ]
+    layout = [[sg.Column(left_width_layout, element_justification='right')],
+              [sg.Column(top_height_layout, element_justification='center'),
+               sg.Column(chroma_camera_layout, element_justification='center')]
+              ]
 
     return layout
